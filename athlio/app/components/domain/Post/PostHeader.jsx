4bc4ec5134fb8cd822285a19f94a5be229aad868
dia-avv <eaa24avv@students.eaaa.dk>
2025-10-29 13:50:43 +0100

    PostHeader component + styling

--- athlio/app/components/domain/Post/PostHeader.jsx
+++ athlio/app/components/domain/Post/PostHeader.jsx
@@ -2,16 +2,19 @@ import ProfilePicture from "../../UI/ProfilePicture";
 import Player from "../../../assets/images/player.jpg";
 import PlusIcon from "../../../assets/icons/plus.svg?react";
 import Button from "../../UI/Button";
+import "./PostHeader.css";
 
 export default function PostHeader({ name, role, date }) {
   return (
-    <div>
-      <ProfilePicture size="medium" verified={true} imgUrl={Player} />
-      <div className="header-text">
-        <p className="name">{name}</p>
-        <div className="subheader-text">
-          <p className="role">{role}</p>
-          <p className="date">{date}</p>
+    <div className="post-header">
+      <div className="header-left">
+        <ProfilePicture size="medium" verified={true} imgUrl={Player} />
+        <div className="header-text">
+          <p className="name">{name}</p>
+          <div className="subheader-text">
+            <p className="role">{role}</p>
+            <p className="date">{date}</p>
+          </div>
         </div>
       </div>
       <Button size="small" type="primary" label="Follow" Icon={PlusIcon} />
