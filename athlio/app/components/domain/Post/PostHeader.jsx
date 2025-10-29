0f55fc628c59f8ce0add7e85e12e751f9bf4a282
dia-avv <eaa24avv@students.eaaa.dk>
2025-10-29 14:53:44 +0100

    PostHeader component + styling + animation

--- athlio/app/components/domain/Post/PostHeader.jsx
+++ athlio/app/components/domain/Post/PostHeader.jsx
@@ -1,10 +1,17 @@
 import ProfilePicture from "../../UI/ProfilePicture";
 import Player from "../../../assets/images/player.jpg";
 import PlusIcon from "../../../assets/icons/plus.svg?react";
+import CheckIcon from "../../../assets/icons/check.svg?react";
 import Button from "../../UI/Button";
 import "./PostHeader.css";
+import { useState } from "react";
 
 export default function PostHeader({ name, role, date }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  function handleFollow() {
+    setIsFollowing((prev) => !prev);
+  }
   return (
     <div className="post-header">
       <div className="header-left">
@@ -17,7 +24,13 @@ export default function PostHeader({ name, role, date }) {
           </div>
         </div>
       </div>
-      <Button size="small" type="primary" label="Follow" Icon={PlusIcon} />
+      <Button
+        size="small"
+        type={isFollowing ? "following" : "primary"}
+        label={isFollowing ? "" : "Follow"}
+        Icon={isFollowing ? CheckIcon : PlusIcon}
+        onClick={handleFollow}
+      />
     </div>
   );
 }
