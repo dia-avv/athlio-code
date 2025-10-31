87f21fb9c68df94539e03dc18da68424b6ca2505
dia-avv <eaa24avv@students.eaaa.dk>
2025-10-31 13:14:39 +0100

    MatchCard component + styling MatchPost component + styling but for the image option

--- athlio/app/routes/home.jsx
+++ athlio/app/routes/home.jsx
@@ -1,5 +1,6 @@
 import BasicPost from "../components/domain/Post/BasicPost";
 import Image from "../assets/images/Image.png";
+import MatchPost from "../components/domain/Post/MatchPost";
 
 export default function Home() {
   return (
@@ -11,6 +12,22 @@ export default function Home() {
         createdAt="21/10"
         content="Important 3 points tonight 🙏⚽. Every game is another chance to grow. Grateful for my family and my teammates for always guiding me through the journey. Let’s keep pushing"
         imageUrl={Image}
+      />
+      <MatchPost
+        id={1}
+        author="Kylian Mbappe"
+        author_role="Midfielder for @RealMadrid"
+        createdAt="21/10"
+        content="Important 3 points tonight 🙏⚽. Every game is another chance to grow. Grateful for my family and my teammates for always guiding me through the journey. Let’s keep pushing"
+        goalsCount={2}
+        assistsCount={5}
+        minCount={45}
+        date="21 Jan"
+        league="La Liga"
+        yourTeam="Real Madrid"
+        opponent="FC Barcelona"
+        yourScore={3}
+        opponentScore={1}
       />
     </>
   );
