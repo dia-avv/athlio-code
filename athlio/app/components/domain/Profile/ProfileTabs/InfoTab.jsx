a78bec1812cb54a662ad760147b1331247f6fb7a
Yuliiakkapi <eaa24yka@students.eaaa.dk>
2025-11-06 14:10:21 +0100

    Matches tab done

--- athlio/app/components/domain/Profile/ProfileTabs/InfoTab.jsx
+++ athlio/app/components/domain/Profile/ProfileTabs/InfoTab.jsx
@@ -1,8 +1,12 @@
+import CardInfoSingle from "../../../UI/InfoCards";
+import "./InfoTab.css";
+
 export default function InfoTab({ profile }) {
   return (
-    <div className="tab-section">
-      <h3>About {profile.full_name}</h3>
-      <p>{profile.bio || "No bio yet."}</p>
-    </div>
+    <main>
+      <div className="profile-info-tab">
+        <CardInfoSingle />
+      </div>
+    </main>
   );
 }
