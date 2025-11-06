41bf68c65f7e3acce26d7546c3c27cbb23ab69da
dia-avv <eaa24avv@students.eaaa.dk>
2025-11-06 12:34:52 +0100

    added button styles for primary but gray

--- athlio/app/routes/layout.jsx
+++ athlio/app/routes/layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import Topbar from "../components/Topbar";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 
 export default function Layout() {
   return (
