0d3e4916cacb34544dfee35e5cbd0aeb4ee06e79
dia-avv <eaa24avv@students.eaaa.dk>
2025-11-10 16:37:53 +0100

    try 18

--- athlio/app/lib/supabase.js
+++ athlio/app/lib/supabase.js
@@ -1,6 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-);
+let supabase = null;
+
+if (typeof window !== "undefined") {
+  const url = import.meta.env.VITE_SUPABASE_URL;
+  const anonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+  if (url && anonKey) {
+    supabase = createClient(url, anonKey);
+  } else {
+    console.warn("Supabase env vars missing!");
+  }
+}
+
+export { supabase };
