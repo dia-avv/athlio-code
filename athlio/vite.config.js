5c8aef637839741b31b1a36346fd7efd97ad3f66
dia-avv <eaa24avv@students.eaaa.dk>
2025-11-11 00:57:13 +0100

    fix: add React and React Router plugins to Vite configuration

--- athlio/vite.config.js
+++ athlio/vite.config.js
@@ -1,4 +1,9 @@
+// vite.config.ts or vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { reactRouter } from "@react-router/dev/vite";
+
 export default defineConfig({
+  plugins: [react(), reactRouter()],
   base: "/athlio-code/",
 });
