d0786564f65bbb2abb5557a35af85abc5464864e
dia-avv <eaa24avv@students.eaaa.dk>
2025-11-10 19:34:46 +0100

    try 31

--- athlio/react-router.config.js
+++ athlio/react-router.config.js
@@ -1,4 +1,8 @@
+/** @type {import('@react-router/dev/config').Config} */
 export default {
-  ssr: false, // SPA build only
-  basename: "/athlio-code", // repo name for GH Pages
+  appDirectory: "app",
+  buildDirectory: "build",
+  ssr: false, // disable SSR since you're deploying a client-only SPA
+  basename: "/athlio-code",
+  prerender: ["/"], // you can remove this entirely if not pre-rendering
 };
