4684859804d1fe11514cb96443fb6b046195af69
dia-avv <eaa24avv@students.eaaa.dk>
2025-11-10 19:47:20 +0100

    try 33

--- athlio/react-router.config.js
+++ athlio/react-router.config.js
@@ -1,8 +1,8 @@
 /** @type {import('@react-router/dev/config').Config} */
 export default {
-  appDirectory: ".",
+  appDirectory: "app",
   buildDirectory: "build",
   ssr: false, // SPA build for GitHub Pages
   basename: "/athlio-code", // your repo name
-  prerender: ["/"], // optional
+  routes: "./routes.js",
 };
