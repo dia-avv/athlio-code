597b35e5cffe2d0b1848f8eb2f4b0dae89002c21
dia-avv <eaa24avv@students.eaaa.dk>
2025-11-11 12:10:05 +0100

    applied patch

--- athlio/react-router.config.js
+++ athlio/react-router.config.js
@@ -1,8 +1,6 @@
 /** @type {import('@react-router/dev/config').Config} */
 
 export default {
-  appDirectory: "app",
-  buildDirectory: "build",
   ssr: false, // SPA build for GitHub Pages
   basename: "/athlio-code",
 };
